869ba8e8fba06321369417b10196c299e6208edb
talhaansari692 <talhaansari692@gmail.com>
2026-03-03 13:43:27 +0530

    resolve new issue 21

--- pages/BulkCall.tsx
+++ pages/BulkCall.tsx
@@ -323,7 +323,7 @@ const BulkCall: React.FC = () => {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                            'Authorization': `Bearer ${localStorage.getItem('vani_access_token')}`
                         },
                         body: JSON.stringify(body),
                         signal: controller.signal,
